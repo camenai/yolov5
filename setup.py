aa40e6fd0f1703947a7289042503863e69555405
Payam Azad <pazad@cyclomedia.com>
2021-03-01 16:03:43 +0100

    add more packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='yolo',
@@ -8,6 +8,6 @@ setup(
     author='Payam Azad',
     author_email='pazad@cyclomedia.com',
     license='unlicense',
-    packages=['yolo'],
+    packages=find_packages(),
     zip_safe=False
 )
