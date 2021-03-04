c1db680525ead0f5956e055bf0e99a58662ce4c8
Payam Azad <pazad@cyclomedia.com>
2021-03-04 07:34:11 +0100

    convert all relative imports back

--- yolov5/models/experimental.py
+++ yolov5/models/experimental.py
@@ -4,10 +4,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from .common import Conv, DWConv
-from ..utils.google_utils import attempt_download
-from .. import models
-from . import yolo
+from models.common import Conv, DWConv
+from utils.google_utils import attempt_download
 
 class CrossConv(nn.Module):
     # Cross Convolution Downsample
